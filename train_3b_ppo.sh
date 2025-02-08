97fcbc6ae52571d69b6222baef33341dafc67ab8
chenpengfei <chenpengfei@lixiang.com>
2025-02-08 22:44:32 +0800

    change model and data path

--- train_3b_ppo.sh
+++ train_3b_ppo.sh
@@ -3,8 +3,8 @@
 export N_GPUS=4
 export CUDA_VISIBLE_DEVICES=2,3
 ray stop --force && ray start --head --include-dashboard=True
-export BASE_MODEL=/lpai/models/Qwen2.5-3B
-export DATA_DIR=/lpai/datasets/countdown
+export BASE_MODEL=models/Qwen2.5-3B
+export DATA_DIR=datasets/countdown
 export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=countdown-qwen2.5-3B
 export VLLM_ATTENTION_BACKEND=XFORMERS
