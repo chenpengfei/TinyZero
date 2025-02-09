bc669c22a81a479b3b883a8844fde48114919339
chenpengfei <chenpengfei@lixiang.com>
2025-02-09 21:28:43 +0800

    并行 GPU 数量改成4

--- train_3b_ppo.sh
+++ train_3b_ppo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export N_GPUS=4
-export CUDA_VISIBLE_DEVICES=2,3
-ray stop --force && ray start --head --include-dashboard=True
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+ray stop --force && ray start --head --include-dashboard=True --num-gpus=4
 export BASE_MODEL=models/Qwen2.5-3B
 export DATA_DIR=datasets/countdown
-export ROLLOUT_TP_SIZE=2
+export ROLLOUT_TP_SIZE=4
 export EXPERIMENT_NAME=countdown-qwen2.5-3B
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
